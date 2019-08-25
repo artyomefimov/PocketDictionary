bc3d16e2a8365857169a4c6f24718cc3d7fa6075
Artyom Efimov <artyomefimov97@gmail.com>
2019-08-25 10:35:27 +0400

    fixed cyrillic input regex #20

--- app/src/main/java/com/artyomefimov/pocketdictionary/utils/InputController.kt
+++ app/src/main/java/com/artyomefimov/pocketdictionary/utils/InputController.kt
@@ -1,7 +1,7 @@
 package com.artyomefimov.pocketdictionary.utils
 
 private val LATIN_INPUT_REGEX = "^[A-Za-z]+[A-Za-z\\s]*".toRegex()
-private val CYRILLIC_INPUT_REGEX = "^[А-яа-я]+[А-яа-я\\s]*".toRegex()
+private val CYRILLIC_INPUT_REGEX = "^[А-яа-яЁё]+[А-яа-яЁё\\s]*".toRegex()
 
 fun isLatinInputCorrect(input: String): Boolean = input.matches(LATIN_INPUT_REGEX)
 fun isLatinInputIncorrect(input: String): Boolean = !isLatinInputCorrect(input)
